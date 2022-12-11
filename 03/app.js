f0601aa3fa6aac966652381edc322caa2e17b19c
Callum Kloos <callum@callums-mbp.lan>
2022-12-11 17:57:43 +0000

    completed 03 part 2

--- 03/app.js
+++ 03/app.js
@@ -34,6 +34,7 @@ for (let line = 0; line < lines.length; line++) {
 	bags.push(bag);
 }
 
+// spot check
 console.log(bags[254]);
 
 // now we want to iterate through the array of bag objects and compare left and right
@@ -124,3 +125,71 @@ for (let bag = 0; bag < bags.length; bag++) {
 	}
 	problemItem = '';
 }
+
+// Part 2
+// Elves are in a group of three, so we want to group this together.
+console.log('Part two starts here:');
+
+let groups = [];
+let group = [];
+for (let bag = 0; bag <= lines.length; bag++) {
+	if (group.length < 3) {
+		group.push(lines[bag]);
+		//	console.log('group length = ' + group.length);
+		//	console.log(group);
+	} else if (group.length == 3) {
+		groups.push(group);
+		group = [];
+		group.push(lines[bag]);
+	}
+}
+console.log('Groups array:');
+console.log(groups);
+console.log(groups.length);
+
+// time to check each group
+// the rule is that only one letter will be in all three strings
+// we want to find this letter.
+
+for (let group = 0; group < groups.length; group++) {
+	let firstElf = groups[group][0];
+	let secondElf = groups[group][1];
+	let thirdElf = groups[group][2];
+
+	// check the first two elves
+	for (let firstElfItem = 0; firstElfItem < firstElf.length; firstElfItem++) {
+		for (let secondElfItem = 0; secondElfItem < secondElf.length; secondElfItem++) {
+			if (firstElf.charAt(firstElfItem) === secondElf.charAt(secondElfItem)) {
+				// then we check the third elf
+				let potentialItem = firstElf.charAt(firstElfItem);
+				for (let thirdElfItem = 0; thirdElfItem < thirdElf.length; thirdElfItem++) {
+					if (potentialItem === thirdElf.charAt(thirdElfItem)) {
+						// this item is in all three elves.
+						console.log('this item');
+						console.log(potentialItem);
+						// add this item to the group array
+						groups[group].push(potentialItem);
+					}
+				}
+			}
+		}
+	}
+}
+
+// now we have gathered the items
+// we can find their priorities
+
+let problemItems = [];
+for (let group = 0; group < groups.length; group++) {
+	problemItems.push(groups[group][3]);
+}
+
+console.log(problemItems);
+console.log(problemItems.length);
+
+totalPriority = 0;
+problemItems.forEach((item) => {
+	totalPriority += itemArray.indexOf(item) + 1;
+});
+
+console.log(totalPriority);
