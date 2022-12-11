8b5a5f9476c95bb74169d3b65de49a123b027261
Callum Kloos <callum@callums-mbp.lan>
2022-12-11 16:22:56 +0000

    competed 03 part 1

--- 03/app.js
+++ 03/app.js
@@ -13,16 +13,15 @@ for (let line = 0; line < lines.length; line++) {
 	let right = '';
 
 	let length = lines[line].length;
-	console.log(length);
+	// console.log(length);
 
 	let halfLength = length / 2 - 1;
-	console.log(halfLength);
+	// console.log(halfLength);
 
 	for (let letter = 0; letter < length; letter++) {
 		if (letter < halfLength) {
 			left += lines[line].charAt(letter);
-			console.log(left);
-		} else if ((letter = halfLength)) {
+		} else if (letter == halfLength) {
 			left += lines[line].charAt(letter);
 			bag.left = left;
 		} else if (letter > halfLength && letter != length - 1) {
@@ -34,4 +33,95 @@ for (let line = 0; line < lines.length; line++) {
 	}
 	bags.push(bag);
 }
-console.log(bags);
+
+console.log(bags[254]);
+
+// now we want to iterate through the array of bag objects and compare left and right
+prioritySum = 0;
+
+// set prioritys
+itemArray = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+];
+
+for (let bag = 0; bag < bags.length; bag++) {
+	let bagCompartment = bags[bag].left;
+	console.log(bagCompartment);
+
+	let checkBagCompartment = bags[bag].right;
+	console.log(checkBagCompartment);
+	let problemItem = '';
+	for (let item = 0; item < bagCompartment.length; item++) {
+		for (let checkItem = 0; checkItem < bagCompartment.length; checkItem++) {
+			if (
+				bagCompartment.charAt(item) === checkBagCompartment.charAt(checkItem) &&
+				bagCompartment.charAt(item) != problemItem &&
+				checkBagCompartment.charAt(checkItem) != problemItem
+			) {
+				// priority is from the array +1.
+				problemItem = bagCompartment.charAt(item);
+				console.log(problemItem);
+
+				let priority = itemArray.indexOf(problemItem) + 1;
+				console.log(priority);
+
+				prioritySum += priority;
+				console.log(prioritySum);
+			} else {
+				// console.log(`item ${item} in bag ${bag} is fine.`);
+			}
+		}
+	}
+	problemItem = '';
+}
