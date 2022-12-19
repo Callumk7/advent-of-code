5c1b83602717dad20135e8afeae1662ac92fb37b
Callum Kloos <callumkloos@gmail.com>
2022-12-19 12:45:36 +0000

    messing about

--- 06/app.js
+++ 06/app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const text = fs.readFileSync('./input.txt', 'utf8');
+const text = fs.readFileSync("./input.txt", "utf8");
 
 // find the character number for part where there are four unrepeated characters
 // iterate along the string to start with.
@@ -9,17 +9,24 @@ const text = fs.readFileSync('./input.txt', 'utf8');
 console.log(text.length); // 4095
 
 // we can start at 03 and work across.
-
 for (let letter = 3; letter < text.length; letter++) {
-	// we want to make a string to test
-	let string = '';
-	for (let i = 0; i <= 3; i++) {
-		string += text.charAt(letter - i);
-	}
-
-	// test string for itself
-	for (let i = 0; i < 3; i++) {
-		if (string.includes(string.charAt(i))) {
-		}
-	}
+    // we want to check for characters that are in the previous 3 spots
+    // This should be easy enough because we can get the position where this
+    // first happened.
+    let string = text.slice(letter - 3, letter);
+    let char = text.charAt(letter);
+    if (!string.includes(char)) {
+        console.log(letter);
+        console.log(`${char} is not in ${string}`);
+        // great! now we need to check this string
+        let stringArray = [];
+        for (let i = 0; i < string.length; i++) {
+            stringArray.push(string.charAt(i));
+        }
+        for (let i = 0; i < string.length; i++) {
+            let letterFromString = stringArray.shift(i);
+            if (!stringArray.includes(letterFromString)) {
+            }
+        }
+    }
 }
